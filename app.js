b33d14424fb43bf02cfbed755d7cb81a577b1950
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-06-16 21:39:45 +0600

    set security HTTP headers

--- app.js
+++ app.js
@@ -4,21 +4,29 @@ const fs = require("fs");
 const appError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const app = express();
 
+// GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
+// Limit request from same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
     message: "Too many request from this IP. Please try again in an hour!",
 });
-
 app.use("/api", limiter);
-app.use(express.json());
+
+// body parser. Reading data from body to req.body
+app.use(express.json({ limit: "2mb" }));
 
 fs.readdirSync("./routes").map((rt) =>
     app.use("/api", require("./routes/" + rt))
