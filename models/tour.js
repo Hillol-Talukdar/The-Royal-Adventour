32e2a3a82fd2086815097e820a3e623d63039a14
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-06-18 02:15:17 +0600

    update tour model with guides via embedding

--- models/tour.js
+++ models/tour.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require("./user");
 
 const tourSchema = new mongoose.Schema(
     {
@@ -101,6 +102,7 @@ const tourSchema = new mongoose.Schema(
                 day: Number,
             },
         ],
+        guides: Array,
     },
     {
         timestamps: true,
@@ -116,6 +118,15 @@ tourSchema.virtual("durationWeeks").get(function () {
 // DOCUMENT MIDDLEWARE. Runs before .create(), .save()
 tourSchema.pre("save", function (next) {
     this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+tourSchema.pre("save", async function (next) {
+    const guidesPromises = this.guides.map(
+        async (id) => await User.findById(id)
+    );
+
+    this.guides = await Promise.all(guidesPromises);
     next();
 });
 
