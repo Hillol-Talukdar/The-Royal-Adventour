22d85782a172dc4a593666b98f37a864e9442693
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-06-17 18:11:54 +0600

    Sanitized data with middlewares

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const appError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const app = express();
 
@@ -27,6 +29,12 @@ app.use("/api", limiter);
 
 // body parser. Reading data from body to req.body
 app.use(express.json({ limit: "2mb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 fs.readdirSync("./routes").map((rt) =>
     app.use("/api", require("./routes/" + rt))
