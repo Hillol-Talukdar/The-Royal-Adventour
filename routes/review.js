5d08a17f35de24917bb073d2cf8ad706ab2d3f61
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-06-20 10:56:40 +0600

    created endpoint for creating and getting route

---
+++ routes/review.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const { getAllReviews, createReviews } = require("../controllers/review");
+ 
+const { protect, restrictTo } = require("../controllers/auth");
+
+router
+    .route("/review")
+    .get(getAllReviews)
+    .post(protect, restrictTo("user"), createReviews);
+
+module.exports = router;
