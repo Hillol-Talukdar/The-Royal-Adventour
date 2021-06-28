2c7222df41b72d704f3121c75e18558b547692f4
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-06-28 17:29:12 +0600

    build and connected tour page with backend

--- controllers/view.js
+++ controllers/view.js
@@ -1,5 +1,6 @@
 const Tour = require("../models/tour");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.getOverview = catchAsync(async (req, res, next) => {
     const tours = await Tour.find();
@@ -10,8 +11,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getTour = (req, res) => {
-    res.status(200).render("tour", {
-        title: "Tour",
+exports.getTour = catchAsync(async (req, res) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: "reviews",
+        field: "review rating user",
     });
-};
+
+    if (!tour) {
+        return next(new AppError("No tour found with that name!", 404));
+    }
+
+    res.status(200).render("tour", {
+        title: `${tour.name} Tour`,
+        tour,
+    });
+});
