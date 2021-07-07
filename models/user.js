cbe6188e27ac24383d71d312f57a5da0cdd1209e
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-07-07 16:36:27 +0600

    uploaded image can save in DB. Added default image for user

--- models/user.js
+++ models/user.js
@@ -23,7 +23,10 @@ const userSchema = new mongoose.Schema(
             enum: ["user", "admin", "lead-guide", "guide"],
             default: "user",
         },
-        photo: String,
+        photo: {
+            type: String,
+            default: "default.jpg",
+        },
         password: {
             type: String,
             required: [true, "Please provide a password"],
