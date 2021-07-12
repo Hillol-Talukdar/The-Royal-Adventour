4660185c5ea96f284e2d8cf1cef89bb192de9593
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-07-12 11:03:17 +0600

    implemented CRUD for booking

--- controllers/booking.js
+++ controllers/booking.js
@@ -2,7 +2,6 @@ const Tour = require("../models/tour");
 const Booking = require("../models/booking");
 const catchAsync = require("../utils/catchAsync");
 const factory = require("./handlerFactory");
-const AppError = require("../utils/appError");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
@@ -50,3 +49,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
     res.redirect(req.originalUrl.split("?")[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getALLBooking = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/booking.js
+++ routes/booking.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCheckoutSession } = require("../controllers/booking");
+const {
+    getCheckoutSession,
+    createBooking,
+    getBooking,
+    getALLBooking,
+    updateBooking,
+    deleteBooking,
+} = require("../controllers/booking");
 
-const { protect } = require("../controllers/auth");
+const { protect, restrictTo } = require("../controllers/auth");
 
 router.get("/booking/checkout-session/:tourId", protect, getCheckoutSession);
+
+router
+    .route("/booking")
+    .get(protect, restrictTo("admin", "lead-guide"), getALLBooking)
+    .post(protect, restrictTo("admin", "lead-guide"), createBooking);
+
+router
+    .route("/booking/:id")
+    .get(protect, restrictTo("admin", "lead-guide"), getBooking)
+    .patch(protect, restrictTo("admin", "lead-guide"), updateBooking)
+    .delete(protect, restrictTo("admin", "lead-guide"), deleteBooking);
 
 module.exports = router;
